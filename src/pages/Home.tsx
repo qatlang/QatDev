69c94076df4804c9d0ba71d6b646ea65b93043e7
Aldrin Mathew <admin@aldrinmathew.com>
2022-10-07 22:44:01 +0530

    Updated `Home`
    - Home has been updated to link to the appropriate pages
    
    - Updated styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,33 @@
 import lavaCapsule from "../media/lava_capsule.png";
 import Button from "../components/Button";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <div className="frontPageContent">
       <div className="frontCarousel">
         <img src={lavaCapsule} className="lavaCover" alt="lava-cover" />
         <div className="phraseBlock">
-          <div className="catchPhrase">Closer to your machine's heart.</div>
+          <div className="catchPhrase">Closer to your machine's heart 🦾</div>
           <div className="description">
-            Modern systems language for efficient & maintainable software...
+            Super-fast modern systems language for efficient & maintainable
+            software...
           </div>
-          <div className="actions">
-            <Button name="Download" onClick={() => {}} />
+          <div className="prompts">
+            <Button
+              content="Download"
+              onClick={() => {
+                navigate("/downloads");
+              }}
+            />
+            <Button
+              content="Try it Online"
+              onClick={() => {
+                navigate("/playground");
+              }}
+            />
           </div>
         </div>
       </div>
