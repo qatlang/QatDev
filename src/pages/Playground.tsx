240bbfe9504f55ee58f76fbf5251cc02c992d315
Aldrin Mathew <admin@aldrinmathew.com>
2022-10-07 22:43:18 +0530

    Added basic code editor in Playground
    - The Playground page now has a basic code editor that also supports
      syntax highlighting

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -1,6 +1,43 @@
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { atomone } from "@uiw/codemirror-theme-atomone";
 import "./Playground.css";
+import Button from "../components/Button";
+
+let editorValue = `main() [
+\tsay "Hello, World!".
+]`;
 
 export default function Playground() {
-  return <div className="playground">Playground</div>;
+  return (
+    <div className="playground">
+      <div className="editorHeader">
+        <Button className="runButton" content="Run" onClick={() => {}} />
+        <Button className="resetButton" content="Reset" onClick={() => {}} />
+      </div>
+      <div className="editorPanel">
+        <ReactCodeMirror
+          className="codeMirror"
+          value={editorValue}
+          basicSetup={{
+            indentOnInput: true,
+            autocompletion: true,
+            bracketMatching: true,
+            crosshairCursor: true,
+            drawSelection: true,
+            highlightActiveLine: true,
+            syntaxHighlighting: true,
+          }}
+          autoFocus={true}
+          indentWithTab={true}
+          height="80vh"
+          width="50vw"
+          theme={atomone}
+          onChange={(val) => {
+            editorValue = val;
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
