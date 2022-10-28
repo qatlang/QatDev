3c9c899b7a18ed65c0ca0e3dc016781df9ac8b78
Aldrin Mathew <admin@aldrinmathew.com>
2022-10-28 19:09:30 +0530

    Added social links to the home page
    - Added social links to the home page
    
    - The added platforms are github, discord and youtube

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
 import lavaCapsule from "../media/lava_capsule.png";
+import discordIcon from "../media/discord.png";
+import githubIcon from "../media/github.png";
+import youtubeIcon from "../media/youtube.png";
 import Button from "../components/Button";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
@@ -28,6 +31,45 @@ export default function Home() {
                 navigate("/playground");
               }}
             />
+          </div>
+          <div className="socialIcons">
+            <div className="githubIcon">
+              <a
+                href="https://github.com/qatlang"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <img
+                  height={40}
+                  width={40}
+                  src={githubIcon}
+                  alt="github-icon"
+                />
+              </a>
+            </div>
+            <div className="discordIcon">
+              <a
+                href="https://discord.gg/CNW3Uvptvd"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <img
+                  height={40}
+                  width={40}
+                  src={discordIcon}
+                  alt="discord-icon"
+                />
+              </a>
+            </div>
+            <div className="youtubeIcon">
+              <a
+                href="https://youtube.com/c/aldrinmathew"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <img width={40} src={youtubeIcon} alt="discord-icon" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
