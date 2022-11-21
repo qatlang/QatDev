0c45272985c98fffba68f73853d372b54c0b541b
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-21 15:44:53 +0530

    Added interface for `LanguageExample`
    - LanguageExample is used to add a new example content to the homepage

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -1,16 +1,21 @@
 export interface ILanguageRelease {
-    version: {
-        value: string,
-        isPrerelease: boolean,
-        prerelease: string
-    },
-    title: string,
-    content: string,
-    files: {
-        platform: string,
-        architecture: string,
-        path: string,
-    }[],
-    index: number,
-    createdAt: string,
+   version: {
+      value: string,
+      isPrerelease: boolean,
+      prerelease: string
+   },
+   title: string,
+   content: string,
+   files: {
+      platform: string,
+      architecture: string,
+      path: string,
+   }[],
+   index: number,
+   createdAt: string,
+}
+
+export interface ILanguageExample {
+   title: string,
+   content: string,
 }
