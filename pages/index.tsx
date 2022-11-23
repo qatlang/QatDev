d247a1dac9f552bee8afd4b10ea40fb70d01cba6
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-23 13:05:05 +0530

    Rehauled UI of the homepage & is now responsive
    - The homepage has been updated so that the examples are displayed in
      the front carousel
    
    - The sponsor info has been moved downwards
    
    - The enire front carousel is now responsive and is properly displayed
      in mobile devices

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import discordIcon from "../public/discord.png";
 import githubIcon from "../public/github.png";
 import youtubeIcon from "../public/youtube.png";
 import aldrinImg from "../public/aldrin.jpeg";
-import Button from "../components/button";
+import Button from "../utils/Button";
 import styles from "styles/Home.module.css";
 import { examples, features } from "../models/data";
 import { useState } from "react";
@@ -11,27 +11,84 @@ import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import Select from "react-select";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import useWindowSize from "../utils/WindowSize";
+import OnlyIf, { valueIf } from "../utils/OnlyIf";
+import joinNames from "../utils/joinNames";
+import fonts from "../utils/fonts";
+
+function Catchphrase() {
+  const win = useWindowSize();
+  return (
+    <div
+      className={joinNames([styles.catchPhrase, fonts.poppins.bold])}
+      style={{
+        fontSize: valueIf(win.isVertical(), "8vmin"),
+        lineHeight: valueIf(win.isVertical(), "8vmin"),
+      }}
+    >
+      {"Closer to your machine's heart 🦾"}
+    </div>
+  );
+}
+
+function LangDescription() {
+  const win = useWindowSize();
+  return (
+    <div
+      className={joinNames([styles.description, fonts.firaCode.normal])}
+      style={{ fontSize: valueIf(win.isVertical(), "3.5vmin") }}
+    >
+      {
+        "Superfast modern systems language for efficient & maintainable software..."
+      }
+    </div>
+  );
+}
 
 export default function Home() {
   const router = useRouter();
+  const win = useWindowSize();
   return (
     <div className={styles.frontPageContent}>
-      <div className={styles.frontCarousel}>
-        <Image
-          src={lavaCapsule}
-          className={styles.lavaCover}
-          alt="lava-cover"
-        />
+      <div
+        className={styles.frontCarousel}
+        style={{ flexDirection: win.isVertical() ? "column" : "row" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: win.isVertical() ? "row" : "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            src={lavaCapsule}
+            style={{
+              height: win.isVertical() ? "40vmin" : "45vmin",
+              width: win.isVertical() ? "40vmin" : "35vmin",
+            }}
+            className={styles.lavaCover}
+            alt="lava-cover"
+          />
+          <OnlyIf If={win.isVertical()}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <Catchphrase />
+              <LangDescription />
+            </div>
+          </OnlyIf>
+        </div>
         <div className={styles.phraseBlock}>
-          <div className={styles.catchPhrase}>
-            {"Closer to your machine's heart 🦾"}
-          </div>
-          <div className={styles.description}>
-            {
-              "Superfast modern systems language for efficient & maintainable software..."
-            }
-          </div>
-          <div className={styles.prompts}>
+          <OnlyIf If={win.isHorizontal()}>
+            <Catchphrase />
+            <LangDescription />
+          </OnlyIf>
+          <div
+            className={joinNames([styles.prompts, fonts.firaCode.bold])}
+            style={{
+              paddingLeft: valueIf(win.isVertical(), "3vmin"),
+            }}
+          >
             <Button
               content="Download"
               onClick={() => {
@@ -45,46 +102,100 @@ export default function Home() {
               }}
             />
           </div>
-          <div className={styles.socialIcons}>
-            <div className={styles.githubIcon}>
+          <div
+            className={styles.socialIcons}
+            style={{ paddingLeft: valueIf(win.isVertical(), "4vmin") }}
+          >
+            <div
+              className={styles.socialIcon}
+              style={{
+                backgroundColor: "#333333",
+                borderRadius: win.isVertical() ? "2.7vmin" : "1.5vmin",
+                height: win.isVertical() ? "10vmin" : "6vmin",
+                width: win.isVertical() ? "10vmin" : "6vmin",
+              }}
+            >
               <a
                 href="https://github.com/qatlang"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 <Image
-                  height={40}
-                  width={40}
+                  height={win.isVertical() ? 30 : 35}
+                  width={win.isVertical() ? 30 : 35}
                   src={githubIcon}
                   alt="github-icon"
                 />
               </a>
             </div>
-            <div className={styles.discordIcon}>
+            <div
+              className={styles.socialIcon}
+              style={{
+                backgroundColor: "#5865f2",
+                borderRadius: win.isVertical() ? "2.7vmin" : "1.5vmin",
+                height: win.isVertical() ? "10vmin" : "6vmin",
+                width: win.isVertical() ? "10vmin" : "6vmin",
+              }}
+            >
               <a
                 href="https://discord.gg/CNW3Uvptvd"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 <Image
-                  height={40}
-                  width={40}
+                  height={win.isVertical() ? 30 : 35}
+                  width={win.isVertical() ? 30 : 35}
                   src={discordIcon}
                   alt="discord-icon"
                 />
               </a>
             </div>
-            <div className={styles.youtubeIcon}>
+            <div
+              className={styles.socialIcon}
+              style={{
+                backgroundColor: "#333333",
+                borderRadius: win.isVertical() ? "2.7vmin" : "1.5vmin",
+                height: win.isVertical() ? "10vmin" : "6vmin",
+                width: win.isVertical() ? "10vmin" : "6vmin",
+              }}
+            >
               <a
                 href="https://youtube.com/c/aldrinmathew"
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <Image width={40} src={youtubeIcon} alt="discord-icon" />
+                <Image
+                  width={win.isVertical() ? 30 : 35}
+                  src={youtubeIcon}
+                  alt="discord-icon"
+                />
               </a>
             </div>
           </div>
         </div>
+        <div
+          className={styles.examplesBlock}
+          style={
+            win.isVertical()
+              ? {
+                  marginTop: "2vmin",
+                  width: "100%",
+                  padding: "2vmin",
+                }
+              : undefined
+          }
+        >
+          <Examples />
+        </div>
+      </div>
+      {/* <div className={styles.sponsorBlock} style={{}}>
+        <div className={joinNames([styles.sponsorHeader, fonts.firaCode.bold])}>
+          <div className={styles.sponsorIcon}>💛</div>
+          Sponsor the project
+        </div>
+        <div className={styles.sponsorContent}></div>
+      </div> */}
+      <div className={styles.secondSection}>
         <div className={styles.sponsorPrompt}>
           <a
             className={styles.githubProfileLink}
@@ -123,18 +234,17 @@ export default function Home() {
             <div className={styles.paypalButton}>💵 PayPal</div>
           </a>
         </div>
-      </div>
-      <div className={styles.secondSection}>
         <div className={styles.featuresBlock}>
-          <p className={styles.featuresTitle}>Key Features</p>
+          <p className={joinNames([styles.featuresTitle, fonts.firaCode.bold])}>
+            Key Features
+          </p>
           <div className={styles.featuresContent}>
-            {/* eslint-disable-next-line react/no-children-prop*/}
-            <ReactMarkdown children={features} />
+            <ReactMarkdown
+              className={joinNames([styles.markdown, fonts.roboto.normal])}
+              // eslint-disable-next-line react/no-children-prop
+              children={features}
+            />
           </div>
-        </div>
-        <div className={styles.examplesBlock}>
-          <p className={styles.examplesTitle}>Examples</p>
-          <Examples />
         </div>
       </div>
     </div>
@@ -143,69 +253,84 @@ export default function Home() {
 
 function Examples() {
   let [active, setActive] = useState(0);
+  const size = useWindowSize();
   return (
     <>
       {
-        <div className={styles.example}>
-          <Select
-            className={styles.exampleSelections}
-            options={examples.flatMap((ex, i) => {
-              return {
-                value: i,
-                label: ex.title,
-              };
-            })}
-            defaultValue={{ value: 0, label: "String Datatype" }}
-            onChange={(val) => {
-              if (val) {
-                setActive(val.value);
-              }
-            }}
-            styles={{
-              menuList: (styles, state) => {
+        <div className={joinNames([styles.example, fonts.firaCode.normal])}>
+          <div className={styles.exampleHeader}>
+            <p
+              className={joinNames([styles.examplesTitle, fonts.firaCode.bold])}
+              style={{ fontSize: size.isVertical() ? "3.5vmin" : undefined }}
+            >
+              Examples
+            </p>
+            <Select
+              className={styles.exampleSelections}
+              options={examples.flatMap((ex, i) => {
                 return {
-                  ...styles,
-                  borderRadius: "5px",
-                  border: "1px solid #555555",
+                  value: i,
+                  label: ex.title,
                 };
-              },
-              option: (styles, state) => {
-                return {
-                  ...styles,
-                  color: "white",
-                  fontWeight: state.isSelected ? "bold" : "normal",
-                };
-              },
-            }}
-            theme={{
-              colors: {
-                primary: "#128f5f",
-                primary75: "#128f5fcc",
-                primary50: "#128f5f99",
-                primary25: "#128f5f55",
-                danger: "#ff0055",
-                dangerLight: "#ff0055",
-                neutral0: "#303030",
-                neutral5: "#555555",
-                neutral10: "#666666",
-                neutral20: "#777777",
-                neutral30: "#888888",
-                neutral40: "#999999",
-                neutral50: "#aaaaaa",
-                neutral60: "#bbbbbb",
-                neutral70: "#cccccc",
-                neutral80: "#dddddd",
-                neutral90: "#ffffff",
-              },
-              borderRadius: 5,
-              spacing: {
-                baseUnit: 2,
-                controlHeight: 0,
-                menuGutter: 0,
-              },
-            }}
-          />
-          <div className={styles.exampleContent}>
+              })}
+              defaultValue={{ value: 0, label: "String Datatype" }}
+              onChange={(val) => {
+                if (val) {
+                  setActive(val.value);
+                }
+              }}
+              styles={{
+                menuList: (styles, state) => {
+                  return {
+                    ...styles,
+                    borderRadius: "5px",
+                    fontSize: size.isVertical()
+                      ? "calc(3.5vmin)"
+                      : "calc(1.5vmin)",
+                    border: "1px solid #555555",
+                  };
+                },
+                option: (styles, state) => {
+                  return {
+                    ...styles,
+                    color: "white",
+                    fontWeight: state.isSelected ? "bold" : "normal",
+                  };
+                },
+              }}
+              theme={{
+                colors: {
+                  primary: "#128f5f",
+                  primary75: "#128f5fcc",
+                  primary50: "#128f5f99",
+                  primary25: "#128f5f55",
+                  danger: "#ff0055",
+                  dangerLight: "#ff0055",
+                  neutral0: "#303030",
+                  neutral5: "#555555",
+                  neutral10: "#666666",
+                  neutral20: "#777777",
+                  neutral30: "#888888",
+                  neutral40: "#999999",
+                  neutral50: "#aaaaaa",
+                  neutral60: "#bbbbbb",
+                  neutral70: "#cccccc",
+                  neutral80: "#dddddd",
+                  neutral90: "#ffffff",
+                },
+                borderRadius: 5,
+                spacing: {
+                  baseUnit: 2,
+                  controlHeight: 0,
+                  menuGutter: 0,
+                },
+              }}
+            />
+          </div>
+          <div
+            className={styles.exampleContent}
+            style={{ fontSize: size.isVertical() ? "3.3vmin" : undefined }}
+          >
             {examples[active].content.split("\n").map((elem, i) => (
               <p className={styles.codeLine} key={"codeLine." + i.toString()}>
                 {elem}
