4b2c52381406aa2f6cd85768460346dd5622bae6
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-26 15:56:38 +0530

    Rehauled homepage
    - Updated homepage so that the sponsor button accompanies the other
      prompts. Currently the sponsor button is not functional
    
    - Added statistics about the project to the homepage, specifically the
      time spent on the project repos in total

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import aldrinImg from "../public/aldrin.jpeg";
 import Button from "../utils/Button";
 import styles from "styles/Home.module.css";
 import { examples, features } from "../models/data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import Select from "react-select";
 import Image from "next/image";
@@ -15,6 +15,7 @@ import useWindowSize from "../utils/WindowSize";
 import OnlyIf, { valueIf } from "../utils/OnlyIf";
 import joinNames from "../utils/joinNames";
 import fonts from "../utils/fonts";
+import { IProjectStats, ITimeStats } from "../models/interfaces";
 
 function Catchphrase() {
   const win = useWindowSize();
@@ -45,9 +46,63 @@ function LangDescription() {
   );
 }
 
+function ProjectTimings(props: { stats: ITimeStats }) {
+  const StatPair = (props: { name: string; value: IProjectStats }) => {
+    return (
+      <div className={styles.projectStatRow}>
+        <div className={styles.projectStatName}>{props.name}</div>
+        <div className={styles.projectStatTime}>
+          {(
+            parseFloat(props.value.data.decimal) +
+            (props.name == "Compiler" ? 400 : 0)
+          ).toString() + " hours"}
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className={joinNames([styles.workStats, fonts.firaCode.normal])}>
+      <div className={styles.workStatsHeader}>
+        <svg
+          className="workStatsIcon"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M10 20.0001C4.48 20.0001 0 15.5301 0 10.0001C0 4.48012 4.48 0.00012207 10 0.00012207C15.53 0.00012207 20 4.48012 20 10.0001C20 15.5301 15.53 20.0001 10 20.0001ZM13.19 13.7101C13.31 13.7801 13.44 13.8201 13.58 13.8201C13.83 13.8201 14.08 13.6901 14.22 13.4501C14.43 13.1001 14.32 12.6401 13.96 12.4201L10.4 10.3001V5.68012C10.4 5.26012 10.06 4.93012 9.65 4.93012C9.24 4.93012 8.9 5.26012 8.9 5.68012V10.7301C8.9 10.9901 9.04 11.2301 9.27 11.3701L13.19 13.7101Z"
+            fill="#cccccc"
+          />
+        </svg>
+        <div className={styles.workStatsTitle}>Time Tracker</div>
+      </div>
+      <StatPair name="Compiler" value={props.stats.compiler} />
+      <StatPair name="Docs" value={props.stats.docs} />
+      <StatPair name="Website" value={props.stats.website} />
+      <StatPair name="Server" value={props.stats.server} />
+      <StatPair name="VSCode ext." value={props.stats.vscode} />
+    </div>
+  );
+}
+
 export default function Home() {
   const router = useRouter();
   const win = useWindowSize();
+  let [compilerStats, setCompilerStats] = useState<ITimeStats | null>(null);
+  useEffect(() => {
+    fetch("/api/workStats", { next: { revalidate: 1 } })
+      .then(async (resp) => {
+        if (resp.status == 200) {
+          setCompilerStats(await resp.json());
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
   return (
     <div className={styles.frontPageContent}>
       <div
@@ -90,17 +145,27 @@ export default function Home() {
             }}
           >
             <Button
-              content="Download"
               onClick={() => {
                 router.push("/downloads");
               }}
-            />
+            >
+              Download
+            </Button>
             <Button
-              content="Try it Online"
               onClick={() => {
                 router.push("/playground");
               }}
-            />
+            >
+              Try Online
+            </Button>
+            <Button
+              className={styles.sponsorIcon}
+              onClick={() => {
+                router.push("/playground");
+              }}
+            >
+              Sponsor / Donate
+            </Button>
           </div>
           <div
             className={styles.socialIcons}
@@ -188,55 +253,18 @@ export default function Home() {
           <Examples />
         </div>
       </div>
-      {/* <div className={styles.sponsorBlock} style={{}}>
+      <div className={styles.sponsorBlock}>
         <div className={joinNames([styles.sponsorHeader, fonts.firaCode.bold])}>
-          <div className={styles.sponsorIcon}>💛</div>
-          Sponsor the project
+          <OnlyIf If={compilerStats != null}>
+            <ProjectTimings stats={compilerStats!} />
+          </OnlyIf>
         </div>
         <div className={styles.sponsorContent}></div>
-      </div> */}
+      </div>
       <div className={styles.secondSection}>
-        <div className={styles.sponsorPrompt}>
-          <a
-            className={styles.githubProfileLink}
-            href="https://github.com/aldrinmathew"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.aldrinImage}
-              src={aldrinImg}
-              alt="aldrin"
-            />
-          </a>
-          <a
-            className={styles.sponsorButtonLink}
-            href="https://github.com/sponsors/aldrinmathew"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className={styles.sponsorButton}>💛 Sponsor</div>
-          </a>
-          <a
-            className={styles.kofiButtonLink}
-            href="https://ko-fi.com/aldrinmathew"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className={styles.kofiButton}>☕ Ko-Fi</div>
-          </a>
-          <a
-            className={styles.paypalButtonLink}
-            href="https://paypal.me/aldrinsartfactory"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className={styles.paypalButton}>💵 PayPal</div>
-          </a>
-        </div>
         <div className={styles.featuresBlock}>
           <p className={joinNames([styles.featuresTitle, fonts.firaCode.bold])}>
-            Key Features
+            Overview
           </p>
           <div className={styles.featuresContent}>
             <ReactMarkdown
