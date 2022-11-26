db3274225cd9dca55b3384b3d61773007c3f84b3
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-26 15:45:22 +0530

    Added `ITimeStats` & `IProjectStats`
    - Added interfaces for time and project statistics from Wakatime

--- models/interfaces.ts
+++ models/interfaces.ts
@@ -19,3 +19,33 @@ export interface ILanguageExample {
    title: string,
    content: string,
 }
+
+export interface IProjectStats {
+   data: {
+      decimal: string;
+      digital: string;
+      is_up_to_date: boolean;
+      percent_calculated: number;
+      project: string;
+      range: {
+         end: string;
+         end_date: string;
+         end_text: string;
+         start: string;
+         start_date: string;
+         start_text: string;
+         timezone: string;
+      };
+      text: string;
+      timeout: number;
+      total_seconds: number;
+   }
+}
+
+export interface ITimeStats {
+   compiler: IProjectStats,
+   website: IProjectStats,
+   server: IProjectStats,
+   vscode: IProjectStats,
+   docs: IProjectStats,
+}
