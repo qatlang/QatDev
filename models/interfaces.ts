4438ee0ac4a97d08f4e169222bb9c953cb68be72
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-28 01:01:24 +0530

    Added `IGitlabRepositoryPushEvent`
    - Added interface for representing the repository push event from Gitlab

--- models/interfaces.ts
+++ models/interfaces.ts
@@ -49,3 +49,60 @@ export interface ITimeStats {
    vscode: IProjectStats,
    docs: IProjectStats,
 }
+
+export interface IGitlabRepositoryPushEvent {
+   object_kind: string,
+   event_name: string,
+   before: string,
+   after: string,
+   ref: string,
+   checkout_sha: string,
+   user_id: number,
+   user_name: string,
+   user_username: string,
+   user_email: string,
+   user_avatar: string,
+   project_id: number,
+   project: {
+      id: number,
+      name: string,
+      description: string,
+      web_url: string,
+      avatar_url: null,
+      git_ssh_url: string,
+      git_http_url: string,
+      namespace: string,
+      visibility_level: number,
+      path_with_namespace: string,
+      default_branch: string,
+      homepage: string,
+      url: string,
+      ssh_url: string,
+      http_url: string
+   },
+   repository: {
+      name: string,
+      url: string,
+      description: string,
+      homepage: string,
+      git_http_url: string,
+      git_ssh_url: string,
+      visibility_level: number
+   },
+   commits:
+   {
+      id: string,
+      message: string,
+      title: string,
+      timestamp: string,
+      url: string,
+      author: {
+         name: string,
+         email: string
+      },
+      added: string[],
+      modified: string[],
+      removed: string[]
+   }[],
+   total_commits_count: number
+}
