d845dcdda8960ea3258c1ca122c8f16e00623b15
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-28 16:26:15 +0530

    Updated repoEvent & added tag notifications
    - The bot should now notify of tags being pushed to gitlab repos
    
    - The client configuration for discord has been moved to next.config.js
      so that it doesn't have to be conditionally done in an endpoint

--- pages/api/repoEvent.ts
+++ pages/api/repoEvent.ts
@@ -1,51 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { IGitlabRepositoryPushEvent } from "../../models/interfaces";
-import { ChannelType, Client, GatewayIntentBits, TextChannel } from 'discord.js';
-import { setTimeout } from "timers/promises";
+import { IGitlabRepositoryPushEvent as IGitlabRepositoryEvent } from "../../models/interfaces";
+import { ChannelType, TextChannel } from 'discord.js';
+let discordConfig = require('../../models/discord');
 
-let discordClient: Client | null = null;
+let allChannels: {
+   repo: TextChannel | null,
+} = { repo: null };
+
+async function getChannels() {
+   if (discordConfig && discordConfig.discordClient && !allChannels.repo) {
+      let repoChan = await discordConfig.discordClient.channels.fetch(process.env['NEXT_PUBLIC_DISCORD_REPO_CHANNEL']?.toString() ?? '');
+      console.log("Got repo channel")
+      if (repoChan && repoChan.type === ChannelType.GuildText) {
+         allChannels.repo = repoChan as TextChannel;
+      }
+   }
+}
 
 export default async function repoEvent(req: NextApiRequest, resp: NextApiResponse) {
+   await getChannels();
    if (req.headers['x-gitlab-token'] === process.env['NEXT_PUBLIC_GITLAB_EVENT_SECRET']) {
       if (req.headers['x-gitlab-event'] === "Push Hook") {
-         if (req.headers['content-type'] === 'application/json') {
-            const pushEvent = req.body as IGitlabRepositoryPushEvent;
-            if (pushEvent.commits.length !== 0) {
-               if (!discordClient) {
-                  discordClient = new Client({
-                     intents: [
-                        GatewayIntentBits.Guilds,
-                        GatewayIntentBits.GuildMembers,
-                        GatewayIntentBits.GuildBans,
-                        GatewayIntentBits.GuildEmojisAndStickers,
-                        GatewayIntentBits.GuildIntegrations,
-                        GatewayIntentBits.GuildWebhooks,
-                        GatewayIntentBits.GuildInvites,
-                        GatewayIntentBits.GuildVoiceStates,
-                        GatewayIntentBits.GuildPresences,
-                        GatewayIntentBits.GuildMessages,
-                        GatewayIntentBits.GuildMessageReactions,
-                        GatewayIntentBits.GuildMessageTyping,
-                        GatewayIntentBits.DirectMessages,
-                        GatewayIntentBits.DirectMessageReactions,
-                        GatewayIntentBits.DirectMessageTyping,
-                        GatewayIntentBits.MessageContent,
-                        GatewayIntentBits.GuildScheduledEvents,
-                        GatewayIntentBits.AutoModerationConfiguration,
-                        GatewayIntentBits.AutoModerationExecution
-                     ]
-                  });
-                  discordClient.login(process.env['NEXT_PUBLIC_DISCORD_BOT_TOKEN']);
-                  await setTimeout(5000);
-               }
-               try {
-                  let repoChannel = await discordClient.channels.fetch(process.env['NEXT_PUBLIC_DISCORD_REPO_CHANNEL']?.toString() ?? '');
-                  if (repoChannel && repoChannel.type === ChannelType.GuildText) {
-                     (repoChannel as TextChannel).send(
-                        `${pushEvent.commits.length.toString()} commit${(pushEvent.commits.length == 1)
-                           ? '' : 's'
-                        } pushed to ***${pushEvent.project.path_with_namespace}*** :: _${pushEvent.ref}_
- ` + "\n" + pushEvent.commits.map((cm) => {
+         if (req.headers['content-type'] !== 'application/json') {
+            console.log("Incorrect content type for request");
+            return resp.status(406).send({});
+         }
+         const pushEvent = req.body as IGitlabRepositoryEvent;
+         if (pushEvent.commits.length !== 0) {
+            try {
+               if (allChannels.repo) {
+                  allChannels.repo.send(
+                     `**\`${pushEvent.project.path_with_namespace}\`**  ::  \`${pushEvent.ref.split('/')[pushEvent.ref.split('/').length - 1]}\`
+${pushEvent.commits.length.toString()} incoming commit${(pushEvent.commits.length == 1)
+                        ? '' : 's'}
+` + "\n" + pushEvent.commits.map((cm) => {
                            return `**${cm.title}**
 <${cm.url}>
 ${cm.message.includes(cm.title) ? cm.message.split(cm.title + '\n')[1] : cm.message}` + (cm.added.length === 0 ? "" : `
@@ -54,18 +42,28 @@ ${cm.message.includes(cm.title) ? cm.message.split(cm.title + '\n')[1] : cm.mess
 🗑️  ${cm.removed.map(val => '*' + val + '*').join(', ')}`) + `
 `;
                         }).join('\n-------------------------------------------------------------------------------------------\n\n') + '\n'
-                        + `by **${pushEvent.user_name}**`
-                     ).catch((e) => console.log("Error while creating the message: ", e))
-                  } else {
-                     console.log("Repo channel could not be retrieved");
-                  }
-               } catch (e) {
-                  console.log("Error while posting repo update: ", e);
+                     + `by **${pushEvent.user_name}**
+
+`
+                  ).catch((e) => console.log("Error while creating the message: ", e))
+               } else {
+                  console.log("Repo channel could not be retrieved");
                }
-               return resp.status(200).send({});
+            } catch (e) {
+               console.log("Error while posting repo update: ", e);
             }
-         } else {
-            console.log("Incorrect content type for request")
+         }
+      } else if (req.headers['x-gitlab-event'] == 'Tag Push Hook') {
+         if (req.headers['content-type'] !== 'application/json') {
+            console.log("Incorrect content type for request");
+            return resp.status(406).send({});
+         }
+         if (allChannels.repo) {
+            const tagEvent = req.body as IGitlabRepositoryEvent;
+            allChannels.repo.send(
+               `**\`${tagEvent.project.path_with_namespace}\`**
+Newly created tag ***${tagEvent.ref.split('/')[tagEvent.ref.split('/').length - 1]}***
+Checkout SHA \`${tagEvent.checkout_sha}\``);
          }
       } else {
          console.log("No correct Gitlab event found")
