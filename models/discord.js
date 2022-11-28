dfb066d7d791f186663bccc8a339b940ca7e3739
Aldrin Mathew <admin@aldrinmathew.com>
2022-11-28 16:24:29 +0530

    Discord configuration is now in next.config.js
    - The discord client is now initialised in next.config.js and the bot is
      logged in. This is so that it doesn't have to be conditionally
      initialised in an endpoint

---
+++ models/discord.js
@@ -0,0 +1,1 @@
+let discordClient = null;
--- next.config.js
+++ next.config.js
@@ -1,8 +1,35 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-   reactStrictMode: true,
-   swcMinify: true,
-}
+const discordJS = require('discord.js');
+const discordConfig = require('./models/discord');
 
-module.exports = nextConfig
+module.exports = async () => {
+   discordConfig.discordClient = new discordJS.Client({
+      intents: [
+         discordJS.GatewayIntentBits.Guilds,
+         discordJS.GatewayIntentBits.GuildMembers,
+         discordJS.GatewayIntentBits.GuildBans,
+         discordJS.GatewayIntentBits.GuildEmojisAndStickers,
+         discordJS.GatewayIntentBits.GuildIntegrations,
+         discordJS.GatewayIntentBits.GuildWebhooks,
+         discordJS.GatewayIntentBits.GuildInvites,
+         discordJS.GatewayIntentBits.GuildVoiceStates,
+         discordJS.GatewayIntentBits.GuildPresences,
+         discordJS.GatewayIntentBits.GuildMessages,
+         discordJS.GatewayIntentBits.GuildMessageReactions,
+         discordJS.GatewayIntentBits.GuildMessageTyping,
+         discordJS.GatewayIntentBits.DirectMessages,
+         discordJS.GatewayIntentBits.DirectMessageReactions,
+         discordJS.GatewayIntentBits.DirectMessageTyping,
+         discordJS.GatewayIntentBits.MessageContent,
+         discordJS.GatewayIntentBits.GuildScheduledEvents,
+         discordJS.GatewayIntentBits.AutoModerationConfiguration,
+         discordJS.GatewayIntentBits.AutoModerationExecution
+      ]
+   });
+   discordConfig.discordClient.login(process.env['NEXT_PUBLIC_DISCORD_BOT_TOKEN']);
+   return {
+      reactStrictMode: true,
+      swcMinify: true,
+   };
+}
