4e734a9fc97272397577317d6513d0dadea99d80
Aldrin Mathew <admin@aldrinmathew.com>
2023-01-28 00:13:25 +0530

    Updated NextJS version & packages
    - Updated version for NextJS, next/font and eslint-config-next

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,10 +6,6 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <Main />
