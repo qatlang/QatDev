8e706e9ed7cbceda97d2dae38f51458d17ef8baa
Aldrin Mathew <admin@aldrinmathew.com>
2023-11-03 20:27:19 +0530

    Updated code examples according to latest syntax
    - All code examples have been updated so that they follow the latest
      syntax of the language

--- models/data.ts
+++ models/data.ts
@@ -1,42 +1,40 @@
 import { type ILanguageExample } from "./interfaces";
 
 export const examples: ILanguageExample[] = [
-   {
-      title: 'Hello world', content:
-         `main() [
+	{
+		title: 'Hello world', content:
+			`pub main() [
    say "Hello, World!".
 ]`
-   },
-   {
-      title: 'Receive CLI arguments',
-      content:
-         `main -> i32
-(#[+ cstring ?] args) [
+	},
+	{
+		title: 'Receive CLI arguments',
+		content:
+			`pub main -> i32
+(args :: multiptr:[cStr]) [
    loop (args'length) : new I [
-      say "Argument at ", I,
-          " is: ", args[I].
+      say "Argument at ", I, " is: ", args[I].
    ]
    give 0.
 ]
  `
-   },
-   {
-      title: 'String Datatype',
-      content:
-         `type String {
-   buffer :: #[+ var u8 'heap].
+	},
+	{
+		title: 'String Datatype',
+		content:
+			`type String {
+   buffer :: multiptr:[var u8 'heap].
    len    :: usize.
    cap    :: usize.
   
- pub:
-   default [
+   pub default [
       ''buffer = null.
       ''len = 0.
       ''cap = 0.
    ]
  
-   from (str val) [
-      ''buffer = heap'get'<u8>(val'length).
+   pub from (str val) [
+      ''buffer = heap'get:[u8](val'length).
       ''len = val'length.
       ''cap = val'length.
       loop (val'length) : new I [
@@ -44,8 +42,8 @@ export const examples: ILanguageExample[] = [
       ]
    ]
  
-   copy other [
-      ''buffer = heap'get'<u8>(other'len).
+   pub copy other [
+      ''buffer = heap'get:[u8](other'len).
       ''len = other'len.
       ''cap = other'cap.
       loop (other'len) : new I [
@@ -53,7 +51,7 @@ export const examples: ILanguageExample[] = [
       ]
    ]
  
-   move other [
+   pub move other [
       ''buffer = other'buffer.
       ''len = other'len.
       ''cap = other'cap.
@@ -62,11 +60,11 @@ export const examples: ILanguageExample[] = [
       other'cap = 0.
    ]
  
-   display() [
+   pub display() [
       say'only str{''buffer, ''len}.
    ]
  
-   end [
+   pub end [
       heap'put(''buffer).
       ''len = 0.
       ''cap = 0.
@@ -74,28 +72,5 @@ export const examples: ILanguageExample[] = [
 }
  
 `
-   },
+	},
 ];
-
-export const features: string =
-   `I know that the capabilities of a programming language cannot be summarised to a few hundred words. But here is a short list of some key features of the language, for those who want to get a basic idea of what the language can or cannot do.
-### RAII
-_**R**esource **A**cquisition **I**s **I**nitialisation_ is a principle in which acquiring/creating a resource initialises it and it is automatically cleaned up when the scope/lifetime of the resource ends. In \`qat\`, this is achieved via Constructors and Destructors.
-
-### Copy & Move semantics
-
-Copy Constructor, Move Constructor, Copy Assignment & Move Assignment. This makes it easy to manage duplication/ownership of data. Both copy & move occurs via references, and in general, is done before an instance is passed to a different context.
-
-### Value Semantics
-
-Value Semantics allows the programmer to use/manage data whose lifetime is not bound to an address.
-This is also used in conjunction with copy & move semantics.
-
-### References
-
-One major advantage of references in \`C++\` is the convenience you get with it, especially when you use operators. References also makes it possible to easily use data without making copies/moves. You get all that convenience in \`qat\`. References are meant to be used when the programmer is sure that the instance referred to outlives the current context of execution.
-
-### Raw Pointers
-
-It's a systems language, of course you want to use pointers. \`qat\` doesn't restrict that in anyway, however, raw pointers has a fresh syntax, in order to pave way to another major feature
-`;
