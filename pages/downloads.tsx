81e5816ebab83ca0136892edaa26112de5949fa8
Aldrin Mathew <admin@aldrinmathew.com>
2023-11-11 00:22:06 +0530

    Updated Downloads page
    - The downloads page has been updated to be more responsive and simpler
    
    - It now shows only the latest release. The ability to switch release
      has been removed for now
    
    - Integrated tailwind-css

--- pages/downloads.tsx
+++ pages/downloads.tsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
-import ReactMarkdown from "react-markdown";
 import { ILanguageRelease } from "../models/interfaces";
-import styles from "styles/Downloads.module.css";
 import WindowsIcon from "public/windows_icon.png";
 import LinuxIcon from "public/linux_icon.png";
 import Image from "next/image";
-import fonts, { FontList } from "../utils/fonts";
-import joinNames from "../utils/joinNames";
-import Select from "react-select";
-import useWindowSize from "../utils/WindowSize";
+// import Select from "react-select";
 import { Env } from "../models/env";
+import Markdown from "../components/Markdown";
+import Button from "../components/Button";
 
 function getIconFromPlatform(platform: string): any {
   if (platform === "windows") {
@@ -47,219 +44,110 @@ export default function Downloads(props: { releases: ILanguageRelease[] }) {
   const releases = props.releases ?? [];
   let [selection, setSelection] = useState(0);
   let [downloadSelection, setDownloadSelection] = useState(0);
-  const updateVersionSelection = (val: number) => {
-    if (downloadSelection >= releases[val].files.length) {
-      setDownloadSelection(0);
-    }
-    setSelection(val);
-  };
-  const size = useWindowSize();
+  let [areDownloadsVisible, setDownloadsVisible] = useState(false);
   return (
-    <div className={styles.downloads}>
+    <div className={"text-[#eeeeee] mx-10 mb-10"}>
       {releases.length > 0 ? (
-        <div className={styles.releaseCard}>
-          <div className={styles.firstColumn}>
-            <div className={styles.releaseSwitcher}>
+        <div className="flex flex-col h-[89vh]">
+          <div className="mb-3 flex flex-row h-fit w-fit align-middle justify-center">
+            <div className="ml-2 font-mono font-bold text-5xl self-center">
+              {(releases[selection].version.value.startsWith("v") ? "" : "v") +
+                releases[selection].version.value}
+            </div>
+            {releases[selection].version.isPrerelease ? (
               <div
-                className={joinNames([
-                  styles.nextVersion,
-                  fonts.jetbrainsMono.bold,
-                ])}
-                style={{
-                  backgroundColor: selection > 0 ? "#128f5f" : "#333333",
-                  color: selection > 0 ? "#ffffff" : "#777777",
-                  cursor: selection > 0 ? "pointer" : "auto",
-                }}
-                onClick={() => {
-                  if (selection > 0) {
-                    updateVersionSelection(selection - 1);
-                  }
-                }}
-              >
-                ++
-              </div>
-              <div
-                className={styles.releaseVersionInfo}
-                style={{
-                  background:
-                    selection == 0
-                      ? `linear-gradient(to bottom right,rgb(0, 50, 255),rgb(255, 0, 80))`
-                      : "#444444",
-                }}
-              >
-                <div className={styles.releaseVersion}>
-                  {releases[selection].version.value}
-                </div>
-                {releases[selection].version.isPrerelease ? (
-                  <div className={styles.releasePrerelease}>
-                    {" " + releases[selection].version.prerelease}
-                  </div>
-                ) : (
-                  <></>
-                )}
-              </div>
-              <div
-                className={joinNames([
-                  styles.previousVersion,
-                  fonts.jetbrainsMono.bold,
-                ])}
+                className="self-center font-mono font-bold rounded-lg h-fit ml-4 mr-6 px-2 py-1 border border-[#ffffff55]"
                 style={{
                   backgroundColor:
-                    selection < releases.length - 1 ? "#128f5f" : "#333333",
-                  color:
-                    selection < releases.length - 1 ? "#ffffff" : "#777777",
-                  cursor: selection < releases.length - 1 ? "pointer" : "auto",
-                }}
-                onClick={() => {
-                  if (selection < releases.length - 1) {
-                    updateVersionSelection(selection + 1);
-                  }
+                    releases[selection].version.prerelease === "alpha"
+                      ? "red"
+                      : releases[selection].version.prerelease === "beta"
+                      ? "blueviolet"
+                      : "green",
                 }}
               >
-                --
+                {releases[selection].version.prerelease}
+              </div>
+            ) : (
+              <></>
+            )}
+            <Button
+              style="h-fit font-mono"
+              onClick={() => {
+                setDownloadsVisible(true);
+              }}
+            >
+              Download
+            </Button>
+          </div>
+          <div className="ml-2 font-mono font-bold text-3xl mb-2 text-left">
+            {releases[selection].title}
+          </div>
+          {areDownloadsVisible && (
+            <div
+              className="flex flex-col align-middle justify-center h-[100vh] w-[100vw] bg-[#000000cc] z-50 fixed left-0 top-0"
+              onClick={() => setDownloadsVisible(false)}
+            >
+              <div className="max-h-[50vh] min-h-[10rem] overflow-y-auto p-5 rounded-2xl bg-black w-fit self-center border-2 border-solid border-[#555555]">
+                {releases[selection].files.flatMap((artefact, i) => {
+                  return (
+                    <div
+                      key={"downloadButton." + i.toString()}
+                      className="self-center w-[30rem] mb-5 px-4 py-2 transition-colors select-none flex flex-row bg-[#128f5f] rounded-xl align-middle justify-start cursor-pointer hover:bg-white hover:text-black active:bg-gray-400"
+                      onClick={() => {
+                        fetch("/api/downloadedRelease", {
+                          method: "POST",
+                          mode: "cors",
+                          body: JSON.stringify({
+                            releaseID: releases[selection].releaseID,
+                            confirmationKey: Env.confirmationKey(),
+                            platformID: artefact.id,
+                          }),
+                          cache: "no-cache",
+                        }).catch((e) => {
+                          console.error(
+                            "Error while incrementing download count",
+                            e
+                          );
+                        });
+                        window.open(artefact.path, "_blank");
+                      }}
+                    >
+                      <div className="flex flex-row">
+                        <Image
+                          className="h-14 w-12 mr-4 self-center"
+                          src={getIconFromPlatform(artefact.platform)}
+                          alt="downloadPlatformIcon"
+                        />
+                        <div className="flex flex-col align-top justify-start text-left">
+                          <div className="mb-2 uppercase font-mono font-bold text-2xl">
+                            {artefact.platform}
+                          </div>
+                          <p className="mb-2">
+                            Architecture:{" "}
+                            <i className="font-mono font-bold bg-[#00000033] py-1 px-2 rounded-lg">
+                              {artefact.architecture}
+                            </i>
+                          </p>
+                          <p>
+                            Target:{" "}
+                            <i className="font-mono font-bold bg-[#00000033] py-1 px-2 rounded-lg">
+                              {artefact.target}
+                            </i>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
             </div>
-            <Select
-              className={joinNames([
-                styles.downloadSelections,
-                fonts.jetbrainsMono.normal,
-              ])}
-              options={releases[selection].files.flatMap((ex, i) => {
-                return {
-                  value: i,
-                  label: ex.platform + " " + ex.architecture,
-                };
-              })}
-              defaultValue={{
-                value: 0,
-                label: [
-                  releases[selection].files[0].platform,
-                  releases[selection].files[0].architecture,
-                ].join(" "),
-              }}
-              onChange={(val) => {
-                if (val) {
-                  setDownloadSelection(val.value);
-                }
-              }}
-              styles={{
-                menuList: (styles, state) => {
-                  return {
-                    ...styles,
-                    borderRadius: "5px",
-                    fontSize: size.isVertical()
-                      ? "calc(3.5vmin)"
-                      : "calc(1.5vmin)",
-                    border: "1px solid #555555",
-                  };
-                },
-                menu: (styles, state) => {
-                  return {
-                    ...styles,
-                    borderRadius: "10px",
-                  };
-                },
-                option: (styles, state) => {
-                  return {
-                    ...styles,
-                    color: "white",
-                    fontFamily: FontList.jetbrainsMono,
-                    fontWeight: state.isSelected ? "bold" : "normal",
-                  };
-                },
-              }}
-              theme={{
-                colors: {
-                  primary: "#128f5f",
-                  primary75: "#128f5fcc",
-                  primary50: "#128f5f99",
-                  primary25: "#128f5f55",
-                  danger: "#ff0055",
-                  dangerLight: "#ff0055",
-                  neutral0: "#303030",
-                  neutral5: "#555555",
-                  neutral10: "#666666",
-                  neutral20: "#777777",
-                  neutral30: "#888888",
-                  neutral40: "#999999",
-                  neutral50: "#aaaaaa",
-                  neutral60: "#bbbbbb",
-                  neutral70: "#cccccc",
-                  neutral80: "#dddddd",
-                  neutral90: "#ffffff",
-                },
-                borderRadius: 5,
-                spacing: {
-                  baseUnit: 2,
-                  controlHeight: 0,
-                  menuGutter: 0,
-                },
-              }}
-            />
-            <div
-              className={joinNames([
-                styles.releaseTitle,
-                fonts.jetbrainsMono.bold,
-              ])}
-            >
-              {releases[selection].title}
-            </div>
-            <ReactMarkdown
-              className={joinNames([
-                styles.releaseContent,
-                fonts.roboto.normal,
-              ])}
-              // eslint-disable-next-line react/no-children-prop
-              children={releases[selection].content}
-            />
-          </div>
-          <div className={styles.secondColumn}>
-            <div className={styles.downloadButtons}>
-              {releases[selection].files.flatMap((artefact, i) => {
-                return (
-                  <div
-                    key={"downloadButton." + i.toString()}
-                    className={styles.downloadButton}
-                    onClick={() => {
-                      console.log("Downloading platform: ", artefact.platform);
-                      fetch("/api/downloadedRelease", {
-                        method: "POST",
-                        mode: "cors",
-                        body: JSON.stringify({
-                          releaseID: releases[selection].releaseID,
-                          confirmationKey: Env.confirmationKey(),
-                          platformID: artefact.id,
-                        }),
-                        cache: "no-cache",
-                      }).catch((e) => {
-                        console.log(
-                          "Error while incrementing download count",
-                          e
-                        );
-                      });
-                      window.open(artefact.path, "_blank");
-                    }}
-                  >
-                    <div className={styles.platformIcon}>
-                      <Image
-                        height={25}
-                        src={getIconFromPlatform(artefact.platform)}
-                        alt="downloadPlatformIcon"
-                      />
-                    </div>
-                    <div className={styles.downloadInfo}>
-                      <div className={styles.downloadPlatform}>
-                        {artefact.platform}
-                      </div>
-                      <div className={styles.downloadArch}>
-                        {artefact.architecture}
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+          )}
+          <Markdown
+            className="mt-4 py-5 px-10 text-left overflow-y-auto rounded-3xl bg-black h-max border-2 border-solid border-[#333333]"
+            // eslint-disable-next-line react/no-children-prop
+            children={releases[selection].content}
+          />
         </div>
       ) : (
         <div>No releases available</div>
