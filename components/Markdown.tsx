aca092c38e543dde3ceffcedf72fe3926335572d
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-21 08:23:45 +0530

    Rehauled `Markdown`
    - Markdown has been rehauled to support light and dark mode
    
    - The copy button for code blocks has been changed to be below the block
    
    - The copy button is now simpler both from code standpoint and from UX
      standpoint. There is no toast notifications anymore if code is copied,
      the button text and color changes instead

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -1,102 +1,69 @@
-import { Ace } from "ace-builds";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
-import { ToastContainer, toast } from "react-toastify";
+import { CodeProps } from "react-markdown/lib/ast-to-react";
 import "react-toastify/dist/ReactToastify.css";
 
 export default dynamic(() => Promise.resolve(Markdown), { ssr: false });
 
-export function Markdown(props: {
-  className?: string;
-  children: string;
-  editor?: Ace.Editor;
-}) {
+export function CodeBlock(props: CodeProps) {
+  let [copied, setCopied] = useState<"copied" | "error" | null>(null);
+  return props.inline ? (
+    <pre className="inline font-bold border border-solid border-gray-400 bg-gray-100 dark:border-gray-600 dark:bg-[#3d434d] rounded-md px-1">
+      {props.children}
+    </pre>
+  ) : (
+    <div className="my-4 flex flex-col">
+      <div className="text-left block overflow-x-auto border-gray-400 bg-[#dce7f9] font-bold dark:border-styleGray dark:bg-[#2f383e] p-3 rounded-t-lg rounded-br-lg border-2 border-solid transition-colors">
+        {props.children}
+      </div>
+      <div
+        className="font-bold place-self-start select-none transition-colors w-fit px-1 py-[0.05rem] bg-gray-600 text-white dark:bg-gray-400 dark:text-black text-sm rounded-b-md cursor-pointer hover:bg-black hover:text-white active:bg-white hover:dark:bg-white hover:dark:text-black active:dark:bg-black"
+        style={{
+          backgroundColor:
+            copied === "copied"
+              ? "#128f5f"
+              : copied === "error"
+              ? "red"
+              : undefined,
+          color: copied ? "white" : undefined,
+        }}
+        onClick={async () => {
+          try {
+            if (navigator && navigator.clipboard && window.isSecureContext) {
+              await navigator.clipboard.writeText(
+                props.children.map((c) => c?.toString()).join()
+              );
+              setCopied("copied");
+              setTimeout(() => {
+                setCopied(null);
+              }, 2000);
+            } else {
+              setCopied("error");
+              setTimeout(() => {
+                setCopied(null);
+              }, 2000);
+            }
+          } catch (e: any) {
+            console.log("Error while copying code block");
+          }
+        }}
+      >
+        {copied ? "✓ Copied" : "Copy"}
+      </div>
+    </div>
+  );
+}
+
+export function Markdown(props: { className?: string; children: string }) {
   return (
     <ReactMarkdown
-      className={props.className + " text-left"}
+      className={props.className + " text-black dark:text-white text-left"}
       components={{
         ul: (value) => <ul className="my-2">{value.children}</ul>,
         li: (value) => <li className="my-2">• {value.children}</li>,
         p: (value) => <p className="inline my-0">{value.children}</p>,
-        code: (value) =>
-          value.inline ? (
-            <pre className="inline bg-[#3d434d] rounded-md px-1">
-              {value.children}
-            </pre>
-          ) : (
-            <div className="my-3 relative text-left block overflow-x-auto bg-[#2b2f36] p-3 rounded-lg border-2 border-solid transition-colors border-[#555555] hover:border-styleGreen">
-              {value.children}
-              <ToastContainer />
-              <div
-                className="select-none transition-colors absolute bottom-1 right-1 px-2 py-1 bg-styleGreen rounded-md cursor-pointer hover:bg-[#2a6b52] active:bg-[#235844]"
-                onClick={async () => {
-                  try {
-                    if (
-                      navigator &&
-                      navigator.clipboard &&
-                      window.isSecureContext
-                    ) {
-                      await navigator.clipboard.writeText(
-                        value.children.map((c) => c?.toString()).join()
-                      );
-                      toast.success<string>("Code copied to clipboard", {
-                        position: "bottom-right",
-                        autoClose: 3000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: false,
-                        draggable: false,
-                        progress: undefined,
-                        theme: "colored",
-                      });
-                    } else {
-                      toast.error<string>(
-                        "Error while copying code: Clipboard could not be found",
-                        {
-                          position: "bottom-right",
-                          autoClose: 3000,
-                          hideProgressBar: false,
-                          closeOnClick: false,
-                          pauseOnHover: true,
-                          draggable: false,
-                          progress: undefined,
-                          theme: "colored",
-                        }
-                      );
-                    }
-                  } catch (e: any) {
-                    toast.error<string>(
-                      "Error while copying code: " + e.toString(),
-                      {
-                        position: "bottom-right",
-                        autoClose: 3000,
-                        hideProgressBar: false,
-                        closeOnClick: false,
-                        pauseOnHover: true,
-                        draggable: false,
-                        progress: undefined,
-                        theme: "colored",
-                      }
-                    );
-                  }
-                }}
-              >
-                <svg
-                  width="19.5"
-                  height="22.5"
-                  viewBox="0 0 156 180"
-                  fill="#ffffff99"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M42 0H78C99.2881 0 116.877 15.838 119.626 36.3736C140.162 39.1229 156 56.7119 156 78V138C156 161.196 137.196 180 114 180H78C56.7119 180 39.123 164.162 36.3736 143.626C15.838 140.877 0 123.288 0 102V42C0 18.804 18.804 0 42 0ZM36 125.244V78C36 54.804 54.804 36 78 36H101.244C98.5796 25.6486 89.183 18 78 18H42C28.7452 18 18 28.7452 18 42V102C18 113.183 25.6486 122.58 36 125.244ZM78 54H114C127.255 54 138 64.7452 138 78V138C138 151.255 127.255 162 114 162H78C64.7452 162 54 151.255 54 138V78C54 64.7452 64.7452 54 78 54Z"
-                  />
-                </svg>
-              </div>
-            </div>
-          ),
+        code: CodeBlock,
       }}
       // eslint-disable-next-line react/no-children-prop
       children={props.children}
