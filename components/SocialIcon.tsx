8e853df6ce812ac46c52eebadf0b7dab22f0e521
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-21 08:20:13 +0530

    SocialIcon supports dark mode

--- components/SocialIcon.tsx
+++ components/SocialIcon.tsx
@@ -7,9 +7,9 @@ export function SocialIcon(props: {
 }) {
   const colorVars = {
     normal:
-      "flex flex-col justify-center align-middle cursor-pointer mx-2 px-2 transition-all border-[3px] border-transparent hover:border-[#ffffff55] rounded-xl sm:h-14 sm:w-14 h-[3.25rem] w-[3.25rem] bg-[#333333]",
+      "flex flex-col justify-center align-middle cursor-pointer mx-2 px-2 transition-all border-[3px] border-transparent hover:border-[#ffffffaa] dark:hover:border-[#ffffff55] rounded-xl sm:h-14 sm:w-14 h-[3.25rem] w-[3.25rem] bg-[#333333]",
     discord:
-      "flex flex-col justify-center align-middle cursor-pointer mx-2 px-2 transition-all border-[3px] border-transparent hover:border-[#ffffff55] rounded-xl sm:h-14 sm:w-14 h-[3.25rem] w-[3.25rem] bg-[#5865f2]",
+      "flex flex-col justify-center align-middle cursor-pointer mx-2 px-2 transition-all border-[3px] border-transparent hover:border-[#ffffffaa] dark:hover:border-[#ffffff55] rounded-xl sm:h-14 sm:w-14 h-[3.25rem] w-[3.25rem] bg-[#5865f2]",
   };
   return (
     <div className={colorVars[props.color ?? "normal"]}>
