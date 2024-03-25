a1b4ad9a8f2c0203fd249d209ca315304ff660a6
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-25 18:20:10 +0530

    Removed unnecessary env var
    - Removed wakatime access token var from ENV

--- models/env.ts
+++ models/env.ts
@@ -5,5 +5,4 @@ export const Env = {
 	discordRepoChannel: () => process.env['NEXT_PUBLIC_DISCORD_REPO_CHANNEL'] ?? '',
 	gitlabEventSecret: () => process.env['NEXT_PUBLIC_GITLAB_EVENT_SECRET'] ?? '',
 	githubEventSecret: () => process.env['NEXT_PUBLIC_GITHUB_EVENT_SECRET'] ?? '',
-	wakatimeAccessToken: () => process.env["NEXT_PUBLIC_WAKATIME_ACCESS_TOKEN"] ?? '',
 }
