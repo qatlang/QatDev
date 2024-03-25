e344a850b27570e66da7b29118d0cc95b5862c4b
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-25 14:11:03 +0530

    Completely rehauled /downloads page
    - Downloads page has been completely rehauled
    
    - The user's platform is taken into consideration, and downloads are
      listed according to that
    
    - Release notes are not completely displayed by default

--- pages/downloads.tsx
+++ pages/downloads.tsx
@@ -1,161 +1,155 @@
 import { useState } from "react";
 import { ILanguageRelease } from "../models/interfaces";
-import WindowsIcon from "public/windows_icon.png";
-import LinuxIcon from "public/linux_icon.png";
-import Image from "next/image";
-// import Select from "react-select";
-import { Env } from "../models/env";
 import Markdown from "../components/Markdown";
-import Button from "../components/Button";
-
-function getIconFromPlatform(platform: string): any {
-  if (platform === "windows") {
-    return WindowsIcon;
-  } else {
-    return LinuxIcon;
-  }
-}
+import pb, { Tables } from "../models/pb";
+import Device from "../components/Device";
 
 export async function getServerSideProps() {
-  const res = await fetch(Env.serverUrl() + "/releases", {
-    method: "GET",
-    mode: "cors",
-    next: { revalidate: 180 },
-  });
-  let jsRes = (await res.json()) as
-    | { releases: ILanguageRelease[] }
-    | undefined;
-  if (jsRes) {
-    return {
-      props: {
-        releases: jsRes.releases.sort(
-          (a: ILanguageRelease, b: ILanguageRelease) => {
-            return b.index - a.index;
-          }
-        ),
-      },
-    };
-  } else {
-    return { props: { releases: [] } };
-  }
+  const result = await pb
+    .collection(Tables.releases)
+    .getFullList<ILanguageRelease>({ sort: "-releaseDate" });
+  return {
+    props: {
+      latestRelease: result,
+    },
+  };
 }
 
-export default function Downloads(props: { releases: ILanguageRelease[] }) {
-  const releases = props.releases ?? [];
-  let [selection, setSelection] = useState(0);
-  let [downloadSelection, setDownloadSelection] = useState(0);
-  let [areDownloadsVisible, setDownloadsVisible] = useState(false);
+function Artefact(props: {
+  value: {
+    id: string;
+    name: string;
+    downloads: number;
+    platform: string;
+    architecture: string;
+    target: string;
+    size: string;
+    path: string;
+  };
+  releaseItemID: string;
+  index: number;
+  artefactCount: number;
+}) {
+  const Info = (props: { name: string; value: string }) => {
+    return (
+      <div className="flex flex-row my-1">
+        <p className="">
+          {props.name + ": "}
+          <b>{props.value}</b>
+        </p>
+      </div>
+    );
+  };
   return (
-    <div className={"flex flex-row text-[#eeeeee] justify-center"}>
-      <title>Downloads | QAT Language</title>
-      {releases.length > 0 ? (
-        <div className="flex flex-col h-[89vh] w-[90%] lg:w-auto mx-10 lg:max-w-[80rem] self-center">
-          <div className="mb-3 flex flex-col sm:flex-row h-fit w-fit align-middle justify-center">
-            <div className="flex flex-row">
-              <div className="ml-2 font-mono font-bold text-3xl sm:text-4xl lg:text-5xl self-center">
-                {(releases[selection].version.value.startsWith("v")
-                  ? ""
-                  : "v") + releases[selection].version.value}
-              </div>
-              {releases[selection].version.isPrerelease ? (
-                <div
-                  className="self-center font-mono font-bold rounded-lg h-fit ml-4 mr-6 px-2 py-1 border border-[#ffffff55]"
-                  style={{
-                    backgroundColor:
-                      releases[selection].version.prerelease === "alpha"
-                        ? "red"
-                        : releases[selection].version.prerelease === "beta"
-                        ? "blueviolet"
-                        : "green",
-                  }}
-                >
-                  {releases[selection].version.prerelease}
-                </div>
-              ) : (
-                <></>
-              )}
-            </div>
-            <Button
-              style="h-fit font-mono mt-3 sm:mt-0"
-              onClick={() => {
-                setDownloadsVisible(true);
-              }}
-            >
-              Download
-            </Button>
+    <div className="flex flex-col p-4 align-top justify-start text-left border-2 border-solid border-gray-300 dark:border-styleGray rounded-2xl shadow-lg dark:shadow-none bg-white dark:bg-black">
+      <div className="mb-1 sm:mb-2 font-bold text-lg sm:text-xl">
+        {props.value.name}
+      </div>
+      <Info name="Target Triple" value={props.value.target} />
+      <Info name="Size" value={props.value.size} />
+      <div
+        key={"downloadButton." + props.index.toString()}
+        className="text-white w-fit mt-2 mb-2 px-3 py-2 font-bold transition-colors select-none flex flex-row bg-[#128f5f] rounded-xl align-middle justify-start cursor-pointer hover:bg-white hover:text-black active:bg-gray-400"
+        onClick={() => {
+          window.open(props.value.path, "_blank");
+        }}
+      >
+        {"Download"}
+      </div>
+    </div>
+  );
+}
+
+export default function Downloads(props: {
+  latestRelease?: ILanguageRelease[];
+}) {
+  const [selection, setSelection] = useState<number>(0);
+  const [showNotes, setShowNotes] = useState<boolean>(false);
+  const release = props.latestRelease![0];
+  const [platform, setPlatform] = useState<string | null>(null);
+  if (!props.latestRelease) {
+    return <div>No releases available</div>;
+  }
+  return (
+    <div className="flex flex-col max-w-[100%] pt-8">
+      <title>Downloads | QAT Programming Language</title>
+      <div className="flex flex-col w-[90%] lg:max-w-[80rem] self-center">
+        <div className="flex flex-row h-fit w-fit align-middle justify-center">
+          <div className="font-mono font-bold rounded-lg bg-white dark:bg-black border-2 border-solid border-gray-300 dark:border-styleGray px-4 py-2 text-xl self-center">
+            v{release.version.value}
           </div>
-          <div className="ml-2 font-mono font-bold text-xl sm:text-2xl lg:text-3xl mb-2 text-left">
-            {releases[selection].title}
-          </div>
-          {areDownloadsVisible && (
-            <div
-              className="flex flex-col align-middle justify-center h-[100vh] w-[100vw] bg-[#000000cc] z-50 fixed left-0 top-0"
-              onClick={() => setDownloadsVisible(false)}
-            >
-              <div className="max-h-[50vh] min-h-[10rem] overflow-y-auto p-5 rounded-2xl bg-black w-fit self-center border-2 border-solid border-[#555555]">
-                {releases[selection].files.flatMap((artefact, i) => {
-                  return (
-                    <div
-                      key={"downloadButton." + i.toString()}
-                      className="self-center w-[70vw] sm:w-[30rem] mb-5 px-4 py-2 transition-colors select-none flex flex-row bg-[#128f5f] rounded-xl align-middle justify-start cursor-pointer hover:bg-white hover:text-black active:bg-gray-400"
-                      onClick={() => {
-                        fetch("/api/downloadedRelease", {
-                          method: "POST",
-                          mode: "cors",
-                          body: JSON.stringify({
-                            releaseID: releases[selection].releaseID,
-                            confirmationKey: Env.confirmationKey(),
-                            platformID: artefact.id,
-                          }),
-                          cache: "no-cache",
-                        }).catch((e) => {
-                          console.error(
-                            "Error while incrementing download count",
-                            e
-                          );
-                        });
-                        window.open(artefact.path, "_blank");
-                      }}
-                    >
-                      <div className="flex flex-row">
-                        <Image
-                          className="sm:h-14 sm:w-12 h-10 w-8 mr-4 self-center"
-                          src={getIconFromPlatform(artefact.platform)}
-                          alt="downloadPlatformIcon"
-                        />
-                        <div className="flex flex-col align-top justify-start text-left">
-                          <div className="mb-1 sm:mb-2 uppercase font-mono font-bold text-xl sm:text-2xl">
-                            {artefact.platform}
-                          </div>
-                          <p className="mb-1 sm:mb-2">
-                            Architecture:{" "}
-                            <i className="font-mono font-bold text-sm sm:text-base bg-[#00000033] py-[0.13rem] sm:py-1 px-1 sm:px-2 rounded-lg">
-                              {artefact.architecture}
-                            </i>
-                          </p>
-                          <p>
-                            Target:{" "}
-                            <i className="font-mono font-bold text-sm sm:text-base bg-[#00000033] py-[0.13rem] sm:py-1 px-1 sm:px-2 rounded-lg">
-                              {artefact.target}
-                            </i>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
+          {release.version.isPrerelease && (
+            <div className="self-center font-mono font-bold text-white bg-red-500 text-lg rounded-lg h-fit ml-4 px-2 py-1">
+              {release.version.prerelease}
             </div>
           )}
-          <Markdown
-            className="mt-4 py-2 sm:py-5 px-5 sm:px-10 text-left overflow-y-auto rounded-3xl bg-black h-max border-2 border-solid border-[#333333]"
-            // eslint-disable-next-line react/no-children-prop
-            children={releases[selection].content}
-          />
         </div>
-      ) : (
-        <div>No releases available</div>
-      )}
+        <Device>
+          {({ isWindows, isMacOs, isDesktop, isMobile }) => {
+            if (platform === "all") {
+              return <></>;
+            }
+            if (isWindows) {
+              setPlatform("windows");
+            } else if (isMacOs) {
+              setPlatform("macOS");
+            } else if (isDesktop) {
+              setPlatform("linux");
+            }
+            return <></>;
+          }}
+        </Device>
+        {platform && platform !== "all" && (
+          <div
+            className="select-none py-1 px-2 rounded-lg transition-colors bg-yellow-400 hover:bg-black dark:hover:bg-white active:bg-white dark:active:bg-black w-fit mt-6 text-left font-bold text-black hover:text-white hover:dark:text-black cursor-pointer active:text-white active:dark:text-black"
+            onClick={() => {
+              setPlatform("all");
+            }}
+          >
+            Looks like your system platform is <b>{platform}</b>. To list all
+            available downloads, click here
+          </div>
+        )}
+        <div className="grid grid-cols-fluid gap-4 mt-6 mb-10">
+          {release.files.flatMap((artefact, i) => {
+            return (
+              (platform
+                ? platform === "all" || artefact.platform === platform
+                : true) && (
+                <Artefact
+                  releaseItemID={release.id}
+                  value={artefact}
+                  index={i}
+                  artefactCount={release.files.length}
+                />
+              )
+            );
+          })}
+        </div>
+        {release.title.length !== 0 && (
+          <div className="mb-2 font-bold text-md sm:text-lg lg:text-xl text-left ">
+            Highlights: {release.title}
+          </div>
+        )}
+        <div
+          className="flex flex-row mb-4"
+          onClick={() => {
+            setShowNotes(!showNotes);
+          }}
+        >
+          <div className="self-center text-lg">Release notes</div>
+          <div className="cursor-pointer ml-2 px-2 py-1 rounded-lg text-white bg-[#555555]">
+            {showNotes ? "🞁" : "🞃"}
+          </div>
+        </div>
+        {showNotes && (
+          <Markdown
+            className="shadow-lg dark:shadow-none text-left rounded-xl h-fit py-2 sm:py-3 px-2 sm:px-5 border-2 border-solid border-gray-300 dark:border-styleGray bg-white dark:bg-black"
+            // eslint-disable-next-line react/no-children-prop
+            children={release.content}
+          />
+        )}
+      </div>
     </div>
   );
 }
