297f5179282c2e36415b24bf02088bebd49c2d9d
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-26 14:15:31 +0530

    Removed unnecessary env vars
    - Removed env vars related  to discord as the discord integration has
      been removed

--- models/env.ts
+++ models/env.ts
@@ -1,8 +1,6 @@
 export const Env = {
 	pocketbaseURL: () => process.env['NEXT_PUBLIC_POCKETBASE_URL'] ?? '',
 	confirmationKey: () => process.env['NEXT_PUBLIC_CONFIRMATION_KEY'] ?? '',
-	discordBotToken: () => process.env['NEXT_PUBLIC_DISCORD_BOT_TOKEN'] ?? '',
-	discordRepoChannel: () => process.env['NEXT_PUBLIC_DISCORD_REPO_CHANNEL'] ?? '',
 	gitlabEventSecret: () => process.env['NEXT_PUBLIC_GITLAB_EVENT_SECRET'] ?? '',
 	githubEventSecret: () => process.env['NEXT_PUBLIC_GITHUB_EVENT_SECRET'] ?? '',
 }
