27fbe4c0753dbede0bafc22d5fb80a4822609769
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-26 12:59:26 +0530

    Updated `/api/images` for adding & removing images
    - The /api/images endpoint can now create and remove images

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import pb, { Tables } from "../../models/pb";
 import http from 'http'
+import { Env } from "../../models/env";
 
 export default async function ImagesHandler(req: NextApiRequest, res: NextApiResponse) {
 	try {
@@ -13,12 +14,28 @@ export default async function ImagesHandler(req: NextApiRequest, res: NextApiRes
 					chunks.push(chunk);
 				});
 				resp.on('end', () => {
-					res.status(200).send(Buffer.concat(chunks));
+					return res.status(200).send(Buffer.concat(chunks));
 				})
 			}).on("error", (e) => {
 				console.log(e.message);
-				res.status(500).send({});
+				return res.status(500).send({});
 			});
+		} else if (req.method === "POST") {
+			const body = JSON.parse(req.body) as { confirmationKey: string; data: FormData };
+			if (body.confirmationKey === Env.confirmationKey()) {
+				const res = await pb.collection(Tables.images).create(body.data)
+				return res.status(200).json(res);
+			} else {
+				return res.status(404).json({});
+			}
+		} else if (req.method === "DELETE") {
+			const body = JSON.parse(req.body) as { confirmationKey: string; id: string };
+			if (body.confirmationKey === Env.confirmationKey()) {
+				const deleted = await pb.collection(Tables.images).delete(body.id);
+				return res.status(200).json({ deleted: deleted });
+			} else {
+				return res.status(404).json({});
+			}
 		} else {
 			return res.status(500).json({});
 		}
