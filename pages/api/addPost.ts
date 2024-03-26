909b73219dcb68790013cb941641feb579599db2
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-26 12:56:26 +0530

    `/api/addPost` endpoint for creating story posts
    - Added API endpoint for creating story posts

---
+++ pages/api/addPost.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import pb, { Tables } from "../../models/pb";
+import { Env } from "../../models/env";
+
+export default async function AddPostHandler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		if (req.method === "POST") {
+			let post: { confirmationKey: string; title: string; content: string; images: string[] } = JSON.parse(req.body) as typeof post;
+			if (post.confirmationKey === Env.confirmationKey()) {
+				const createRes = await pb.collection(Tables.story).create({
+					title: post.title,
+					content: post.content,
+					images: post.images,
+					source: "site",
+					timestamp: new Date().toUTCString(),
+				});
+				return res.status(200).json(createRes);
+			} else {
+				return res.status(404).json({});
+			}
+		} else {
+			return res.status(500).json({});
+		}
+	} catch (e) {
+		return res.status(500).json({});
+	}
+}
