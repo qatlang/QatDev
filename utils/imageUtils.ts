63faa7bbc82e66505ddfe4290ac6be734343cc7d
Aldrin Mathew <admin@aldrinmathew.com>
2024-03-26 13:53:18 +0530

    Updated `addImage` and `removeImageByID`
    - addImage has been updated to fix errors

--- utils/imageUtils.ts
+++ utils/imageUtils.ts
@@ -1,12 +1,27 @@
 import { RecordModel } from "pocketbase";
-import pb, { Tables } from "../models/pb";
+import { Env } from "../models/env";
 
-export async function addImageFromBlob(name: string, blob: Blob): Promise<RecordModel> {
-	let imageData = new FormData();
-	imageData.append("file", blob, name);
-	return await pb.collection(Tables.images).create(imageData);
+export async function addImageToDB(name: string, dataURL: string): Promise<RecordModel | null> {
+	let res = await fetch('/api/images', {
+		method: 'POST',
+		body: JSON.stringify({ confirmationKey: Env.confirmationKey(), name: name, dataURL: dataURL }),
+		cache: 'no-store'
+	});
+	if (res.status === 200) {
+		return (await res.json()) as RecordModel;
+	} else {
+		return null;
+	}
 }
 
-export function removeImageByID(id: string) {
-	pb.collection(Tables.images).delete(id, { requestKey: null });
+export async function removeImageByID(id: string): Promise<boolean> {
+	let res = await fetch('/api/images', {
+		method: 'DELETE',
+		body: JSON.stringify({ confirmationKey: Env.confirmationKey(), id: id })
+	})
+	if (res.status === 200) {
+		return (await res.json() as { deleted: boolean }).deleted;
+	} else {
+		return false;
+	}
 }
